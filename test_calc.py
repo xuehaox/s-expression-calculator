bb893d3802fed033e72af37927659e6168724dcd
prism342 <66716445+prism342@users.noreply.github.com>
2023-02-18 19:04:31 -0500

    update testing code

--- test_calc.py
+++ test_calc.py
@@ -1,95 +1,82 @@
 #!/usr/bin/env python3
 
-import sys 
+import sys
 import unittest
 from subprocess import check_output
 
 
-def run_calc(expr: str) -> str:
-    return check_output('python3 ./calc.py {}'.format(expr), shell=True).decode(sys.stdout.encoding).strip()
+TEST_CASES = [
+    ("0",
+     0
+     ),
 
-class TestLexer(unittest.TestCase):
-    
-    def test_calc(self):
+    ("13147483647",
+     13147483647
+     ),
 
-        self.assertEqual(
-            run_calc("0"), 
-            str(0)
-        )
+    ("(add 1547483647 1547483647)",
+     1547483647 + 1547483647
+     ),
 
-        self.assertEqual(
-            run_calc("13147483647"), 
-            str(13147483647)
-        )
+    ("(add 1 1)",
+     2
+     ),
 
-        self.assertEqual(
-            run_calc('"(add 1547483647 1547483647)"'), 
-            str(1547483647 + 1547483647)
-        )
+    ("(add 123 456)",
+     579
+     ),
 
-        self.assertEqual(
-            run_calc('"(add 1 1)"'), 
-            str(2)
-        )
+    ("(add 0 (add 3 4))",
+     7
+     ),
 
-        self.assertEqual(
-            run_calc('"(add 123 456)"'), 
-            str(579)
-        )
+    ("(add 3 (add (add 3 3) 3))",
+     12
+     ),
 
-        self.assertEqual(
-            run_calc('"(add 0 (add 3 4))"'), 
-            str(7)
-        )
+    ("(multiply 1 1)",
+     1
+     ),
 
-        self.assertEqual(
-            run_calc('"(add 3 (add (add 3 3) 3))"'), 
-            str(12)
-        )
+    ("(multiply 0 (multiply 3 4))",
+     0
+     ),
 
-        self.assertEqual(
-            run_calc('"(multiply 1 1)"'), 
-            str(1)
-        )
+    ("(multiply 3 (multiply (multiply 3 3) 3))",
+     81
+     ),
 
-        self.assertEqual(
-            run_calc('"(multiply 0 (multiply 3 4))"'), 
-            str(0)
-        )
+    ("(add 1 (multiply 2 3))",
+     7
+     ),
 
-        self.assertEqual(
-            run_calc('"(multiply 3 (multiply (multiply 3 3) 3))"'), 
-            str(81)
-        )
+    ("(multiply 2 (add (multiply 2 3) 8))",
+     28
+     ),
 
-        self.assertEqual(
-            run_calc('"(add 1 (multiply 2 3))"'), 
-            str(7)
-        )
+    ("(multiply (add 1 2) 3)",
+     9
+     ),
 
-        self.assertEqual(
-            run_calc('"(multiply 2 (add (multiply 2 3) 8))"'), 
-            str(28)
-        )
+    ("(multiply 2 (add (multiply (multiply 2 (add (multiply 2 (multiply 2 (add (multiply 2 3) 8))) 8)) 3) 8))",
+     784
+     ),
+]
 
-        self.assertEqual(
-            run_calc('"(multiply (add 1 2) 3)"'), 
-            str(9)
-        )
 
-        self.assertEqual(
-            run_calc('"(multiply 2 (add (multiply (multiply 2 (add (multiply 2 (multiply 2 (add (multiply 2 3) 8))) 8)) 3) 8))"'), 
-            str(784)
-        )
+def runCalc(expr: str) -> str:
+    return check_output('python ./calc.py "{}"'.format(expr), shell=True).decode(sys.stdout.encoding).strip()
 
-        # test running time
-        """
-        self.assertEqual(
-            run_calc('"(multiply (multiply 2 (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply (multiply (multiply 2 (multiply 3 (multiply 2 (add (multiply 2 (multiply 2 (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8))))))) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8)))))) (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8)))))) (add (multiply (multiply 2 (add (multiply 2 3) 8)) (multiply 2 (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply (multiply (multiply 2 (multiply 3 (multiply 2 (add (multiply 2 (multiply 2 (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8))))))) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8)))))) (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8)))))) (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8))))))) 8)) (add (multiply 2 3) 8)))))) (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply (multiply (multiply 2 (multiply 3 (multiply 2 (add (multiply 2 (multiply 2 (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8))))))) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8)))))) (multiply 3 (multiply 2 (add (multiply 2 3) (multiply (multiply 2 (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8)))))) (add (multiply (multiply 2 (add (multiply 2 3) 8)) 3) 8)) (add (multiply 2 3) 8))))))"'), 
-            str(0)
-        )
-        """
 
+class TestCalc(unittest.TestCase):
+
+    def testCalc(self):
+        for testCase in TEST_CASES:
+            testInput, result = testCase
+            self.assertEqual(
+                runCalc(testInput),
+                str(result)
+            )
 
 
 if __name__ == '__main__':
